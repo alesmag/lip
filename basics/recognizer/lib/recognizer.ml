e3c194658680965c3ed1321773bba5ccbfe619f9
alesmag <alessiog.mag@tiscali.it>
2024-11-06 22:53:05 +0100

    modifica

--- basics/recognizer/lib/recognizer.ml
+++ basics/recognizer/lib/recognizer.ml
@@ -1,6 +1,17 @@
-let lang1 _ = failwith ""
+let rec lang1 list = match list with
+  | [x] when (x == '0') || (x == '1') -> true
+  | x::list when (x == '0') || (x == '1') -> true && lang1 list
+  | _ -> false
+;;
 
-let lang2 _ = failwith ""
+let rec lang2_rec list = match list with
+  | [] -> true
+  | [x] when (x == '0') || (x == '1') -> true
+  | '1'::list -> true && lang2_rec list
+  | _ -> false
+;;
+
+let lang2 list = lang2_rec(List.rev(list));;
 
 let lang3 _ = failwith ""
 
