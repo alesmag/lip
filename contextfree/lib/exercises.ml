486e58433c89ff4c19843a37a113bca16a94d921
alesmag <alessiog.mag@tiscali.it>
2024-11-20 16:15:32 +0100

    modifiche

--- contextfree/lib/exercises.ml
+++ contextfree/lib/exercises.ml
@@ -16,15 +16,54 @@ let todo : grammar =
 
 
 (* #### Exercise 1, easy (zero_n_one_n) *)
-let zero_n_one_n : grammar = todo
+let zero_n_one_n : grammar = 
+  {
+    symbols = [ S ];
+    terminals = [ '0'; '1' ];
+    productions = 
+      [
+        S --> "0S1";
+        S --> "";
+      ];
+    start = S;
+  }
+;;
 
 
 (* #### Exercise 2, easy (palindromes) *)
-let palindromes : grammar = todo
+let palindromes : grammar = 
+  {
+    symbols = [ S ];
+    terminals = [ '0'; '1' ];
+    productions = 
+      [
+        S --> "0S0";
+        S --> "1S1";
+        S --> "0";
+        S --> "1";
+        S --> "";
+      ];
+    start = S;
+  }
+;;
 
 
 (* #### Exercise 3, medium (balanced_parentheses) *)
-let balanced_parentheses : grammar = todo
+let balanced_parentheses : grammar = 
+  {
+    symbols = [ S ];
+    terminals = [ '('; ')'; '['; ']'; '{'; '}' ];
+    productions = 
+      [
+        S --> "SS";
+        S --> "(S)";
+        S --> "[S]";
+        S --> "{S}";
+        S --> "";
+      ];
+    start = S;
+  }
+;;
 
 
 (* #### Exercise 4, hard (same_amount)
@@ -32,4 +71,18 @@ let balanced_parentheses : grammar = todo
    Hint: model the language of words where the number of 0's is
    one greater than the number of 1's and viceversa, then combine them.
 *)
-let same_amount : grammar = todo
+let same_amount : grammar = 
+  {
+    symbols = [ S ];
+    terminals = [ '0'; '1'; ];
+    productions = 
+      [
+        S --> "0S0";
+        S --> "1S1";
+        S --> "0S1";
+        S --> "1S0";
+        S --> "";
+      ];
+    start = S;
+  }
+;;
--- contextfree/test/test_contextfree.ml
+++ contextfree/test/test_contextfree.ml
@@ -14,22 +14,23 @@ let%test "example_test" = derive todo [ 0; 1; 0; 1; 2 ] |> to_string = "01011010
 *)
 
 (* #### Exercise 1 *)
-let%test "zero_n_one_n_1" = derive zero_n_one_n [ (* FILL IN HERE *) ] |> to_string = ""
-let%test "zero_n_one_n_2" = derive zero_n_one_n [ (* FILL IN HERE *) ] |> to_string = "01"
-let%test "zero_n_one_n_3" = derive zero_n_one_n [ (* FILL IN HERE *) ] |> to_string = "00000000001111111111"
+let%test "zero_n_one_n_1" = derive zero_n_one_n [ 1; ] |> to_string = ""
+let%test "zero_n_one_n_2" = derive zero_n_one_n [ 0; 1; ] |> to_string = "01"
+let%test "zero_n_one_n_3" = derive zero_n_one_n [ 0; 0; 0; 0; 0; 0; 0; 0; 0; 0; 1; ] |> to_string = "00000000001111111111"
 
 (* #### Exercise 2 *)
-let%test "palindromes_1" = derive palindromes [ (* FILL IN HERE *) ] |> to_string = "11011"
-let%test "palindromes_2" = derive palindromes [ (* FILL IN HERE *) ] |> to_string = "0110"
-let%test "palindromes_3" = derive palindromes [ (* FILL IN HERE *) ] |> to_string = "011101110"
+let%test "palindromes_1" = derive palindromes [ 1; 1; 2; ] |> to_string = "11011"
+let%test "palindromes_2" = derive palindromes [ 0; 1; 4; ] |> to_string = "0110"
+let%test "palindromes_3" = derive palindromes [ 0; 1; 1; 1; 2; ] |> to_string = "011101110"
 
 (* #### Exercise 3 *)
-let%test "balanced_parentheses_1" = derive balanced_parentheses [ (* FILL IN HERE *) ] |> to_string = "()[]{}"
-let%test "balanced_parentheses_2" = derive balanced_parentheses [ (* FILL IN HERE *) ] |> to_string = "({})[]"
-let%test "balanced_parentheses_3" = derive balanced_parentheses [ (* FILL IN HERE *) ] |> to_string = "({[][{}()]})"
+let%test "balanced_parentheses_1" = derive balanced_parentheses [ 0; 0; 1; 4; 2; 4; 3; 4; ] |> to_string = "()[]{}"
+let%test "balanced_parentheses_2" = derive balanced_parentheses [ 0; 1; 3; 4; 2; 4; ] |> to_string = "({})[]"
+let%test "balanced_parentheses_3" = derive balanced_parentheses [ 1; 3; 0; 2; 4; 2; 0; 3; 4; 1; 4; ] |> to_string = "({[][{}()]})"
+
 
 (* #### Exercise 4 *)
-let%test "zero_one_same_1" = derive same_amount [ (* FILL IN HERE *) ] |> to_string = ""
-let%test "zero_one_same_2" = derive same_amount [ (* FILL IN HERE *) ] |> to_string = "1001"
-let%test "zero_one_same_3" = derive same_amount [ (* FILL IN HERE *) ] |> to_string = "00110101"
-let%test "zero_one_same_4" = derive same_amount [ (* FILL IN HERE *) ] |> to_string = "10001110"
+let%test "zero_one_same_1" = derive same_amount [ 4; ] |> to_string = ""
+let%test "zero_one_same_2" = derive same_amount [ 1; 0; 4; ] |> to_string = "1001"
+let%test "zero_one_same_3" = derive same_amount [ 2; 0; 1; 3; 4; ] |> to_string = "00110101"
+let%test "zero_one_same_4" = derive same_amount [ 3; 2; 2; 2; 4; ] |> to_string = "10001110"
