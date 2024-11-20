bfc7130bbda1406864f6e517a1f709ca87a43bfe
alesmag <alessiog.mag@tiscali.it>
2024-11-20 16:31:56 +0100

    modifiche + toylexer da completare

--- toylexer/lib/main.ml
+++ toylexer/lib/main.ml
@@ -24,4 +24,6 @@ let string_of_frequencies fl =
   List.fold_left (fun s (t,n) -> s ^ ((string_of_token t) ^ " -> " ^ string_of_int n ^ "\n")) "" fl
 
 (* frequency : int -> 'a list -> ('a * int) list *)
-let frequency _ _ = failwith("TODO")
+let frequency n lst = 
+  failwith("TODO")
+;;
