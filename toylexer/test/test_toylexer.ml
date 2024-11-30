9d60b4ba9862b42861eeef694dd045f9e59d33a7
alesmag <alessiog.mag@tiscali.it>
2024-11-30 16:50:50 +0100

    fine task 3

--- toylexer/test/test_toylexer.ml
+++ toylexer/test/test_toylexer.ml
@@ -2,6 +2,21 @@ open Toylexer.Token
 open Toylexer.Main
 
 let%test "test_frequencies_1" =
-  lexer "x=y; x=x+1" |> frequency 3 = [(ID "x", 3); (ASSIGN, 2); (ID "y", 1)]
+  lexer "x=y; x=x+1" |> frequency 3 = [(CTOK "x", 3); (ASSIGN, 2); (CTOK "y", 1)]
 
 (* YOUR TESTS HERE *)
+
+let%test "test_atok" =
+  lexer "Ciao a tutti" |> frequency 3 = [(ATOK "Ciao", 1); (BTOK "a", 1); (ID "tutti", 1)]
+
+let%test "test_btok" =
+  lexer "a ieaei a o" |> frequency 3 = [(BTOK "a", 2); (BTOK "ieaei", 1); (BTOK "o", 1)]
+
+let%test "test_ctok" =
+  lexer "figaro lally chap" |> frequency 3 = [(ID "figaro", 1); (CTOK "lally", 1); (CTOK "chap", 1)]
+
+let%test "test_dtok" =
+  lexer "-3.14 7 94." |> frequency 3 = [(DTOK "-3.14", 1); (DTOK "7", 1); (DTOK "94.", 1)]
+
+let%test "test_etok" =
+  lexer "0x34 0X00 0x5D" |> frequency 3 = [(ETOK "0x34", 1); (ETOK "0X00", 1); (ETOK "0x5D", 1)]
