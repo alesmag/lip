22598309e2e42cae2db7bc0a9d2b52afa4814b45
alesmag <alessiog.mag@tiscali.it>
2024-12-03 21:03:27 +0100

    fine task 1

--- life/bin/main.ml
+++ life/bin/main.ml
@@ -1,12 +1,14 @@
 module T = ANSITerminal
 open Life.Main
 
+let rule_s23_b3 = { Life.Main.survive = [2; 3]; Life.Main.birth = [3] }
+
 let _ = match Array.length(Sys.argv) with
     2 -> let k = int_of_string (Sys.argv.(1)) in
     T.erase T.Screen;
     T.save_cursor();
     Random.self_init();
-    let w = loop init_w k in
+    let w = loop init_w k rule_s23_b3 in
     display w;
     ignore(read_line());
     T.restore_cursor();
--- life/lib/main.ml
+++ life/lib/main.ml
@@ -1,6 +1,8 @@
 module T = ANSITerminal
 open Printf
 
+type rule = { survive: int list; birth: int list }
+
 (* let rec range a b = if b<a then [] else a::(range (a+1) b) *)
 
 let rec zeroes = function
@@ -46,23 +48,23 @@ let count1 l = List.fold_left (fun s x -> s + (if x then 1 else 0)) 0 l
 
 let count w = List.fold_left (fun s x -> s + count1 x) 0 w
 
-let alive w i j =
+let alive w i j rule =
   let (cell,nb) = neighbours w i j in
   let alive_nb = count nb in
   if cell then (* cell is alive *)
     (* cell survives? *)
-    alive_nb = 2 || alive_nb = 3
+    List.mem alive_nb rule.survive
   else (* cell is dead *)
     (* cell is born? *)
-    alive_nb = 3
+    List.mem alive_nb rule.birth
 
-let step1 w i =
+let step1 w i rule =
   let n = List.length w in
-  List.mapi (fun j _ -> alive w i j) (zeroes n)
+  List.mapi (fun j _ -> alive w i j rule) (zeroes n)
 
-let step w =
+let step w rule =
   let n = List.length w in
-  List.mapi (fun i _ -> step1 w i) (zeroes n)
+  List.mapi (fun i _ -> step1 w i rule) (zeroes n)
 
 (* let step w = List.map step1 w *)
 (* let step w = w *)
@@ -74,6 +76,6 @@ let display w =
   printf "%s\n%!" (string_of_world w);
   Unix.sleepf 0.15;;
 
-let rec loop w n =
+let rec loop w n rule =
   if n=0 then (display w; w)
-  else (display w; loop (step w) (n-1))
+  else (display w; loop (step w rule) (n-1) rule)
