59e79430a3083b9d29b710586e922d245139d35f
alesmag <alessiog.mag@tiscali.it>
2024-12-17 12:37:29 +0100

    fine

--- expr/sarithexpr/lib/ast.ml
+++ expr/sarithexpr/lib/ast.ml
@@ -1,3 +1,5 @@
+exception TypeError of string
+
 type expr =
     True
   | False
@@ -14,11 +16,39 @@ type exprval =
   | Bool of bool
   | Nat of int
 
+type exprtype = 
+  | BoolT
+  | NatT
+
+
+let rec string_of_expr = function
+  | True -> "True"
+  | False -> "False"
+  | If(e0,e1,e2) -> "If(" ^ (string_of_expr e0) ^ "," ^ (string_of_expr e1) ^ "," ^ (string_of_expr e2) ^ ")"
+  | Not e -> "Not(" ^ (string_of_expr e) ^ ")"
+  | And(e1, e2) -> "And(" ^ (string_of_expr e1) ^ "," ^ (string_of_expr e2) ^ ")"
+  | Or(e1, e2) -> "Or(" ^ (string_of_expr e1) ^ "," ^ (string_of_expr e2) ^ ")"
+  | Zero -> "0"
+  | Pred(e) -> "Pred(" ^ (string_of_expr e) ^ ")"
+  | Succ(e) -> "Succ(" ^ (string_of_expr e) ^ ")"
+  | IsZero(e) -> "IsZero(" ^ (string_of_expr e) ^ ")"
+
+
 let string_of_val = function
   | Bool b -> string_of_bool b
   | Nat n -> string_of_int n
+
+
+let string_of_type = function
+  | BoolT -> "BoolT"
+  | NatT -> "NatT"
+
 
 let rec is_nv = function
   | Zero -> true
   | Succ e -> is_nv(e) 
   | _ -> false
+
+
+let raise_type_error current expect expr =
+  raise (TypeError ((string_of_expr expr) ^ " has type " ^ current ^ ", but type " ^ expect ^ " was expected."))
--- expr/sarithexpr/lib/main.ml
+++ expr/sarithexpr/lib/main.ml
@@ -1,18 +1,5 @@
 open Ast
 
-let rec string_of_expr = function
-  | True -> "True"
-  | False -> "False"
-  | If(e0,e1,e2) -> "If(" ^ (string_of_expr e0) ^ "," ^ (string_of_expr e1) ^ "," ^ (string_of_expr e2) ^ ")"
-  | Not e -> "Not(" ^ (string_of_expr e) ^ ")"
-  | And(e1, e2) -> "And(" ^ (string_of_expr e1) ^ "," ^ (string_of_expr e2) ^ ")"
-  | Or(e1, e2) -> "Or(" ^ (string_of_expr e1) ^ "," ^ (string_of_expr e2) ^ ")"
-  | Zero -> "0"
-  | Pred(e) -> "Pred(" ^ (string_of_expr e) ^ ")"
-  | Succ(e) -> "Succ(" ^ (string_of_expr e) ^ ")"
-  | IsZero(e) -> "IsZero(" ^ (string_of_expr e) ^ ")"
-
-
 let parse (s : string) : expr =
   let lexbuf = Lexing.from_string s in
   let ast = Parser.prog Lexer.read lexbuf in
@@ -53,49 +40,86 @@ let rec eval : expr -> exprval = function
   | True -> Bool true
   | False -> Bool false
   | Zero -> Nat 0
-
   | If(e1,e2,e3) -> (
       match eval e1 with
       | Bool true -> eval e2
       | Bool false -> eval e3
       | _ -> failwith "Error If"
     )
-
   | Not e -> (
       match eval e with
       | Bool b -> Bool (not b)
       | _ -> failwith "Error Not"
     )
-  
   | And(e1, e2) -> (
       match eval e1 with
       | Bool true -> eval e2
       | Bool false -> Bool false
       | _ -> failwith "Error And"
     )
-
   | Or(e1, e2) -> (
       match eval e1 with
       | Bool true -> Bool true
       | Bool false -> eval e2
       | _ -> failwith "Error Or"
     )
-
   | Succ(e) -> (
       match eval e with 
       | Nat n -> Nat (n + 1)
       | _ -> failwith "Error Succ"
     ) 
-
   | Pred(e) -> (
       match eval e with 
       | Nat n when (n > 0) -> Nat (n - 1)
       | _ -> failwith "Error Pred"
     )
-
   | IsZero(e) -> (
       match eval e with
       | Nat 0 -> Bool true
       | Nat _ -> Bool false
       | _ -> failwith "Error IsZero"
     )
+
+
+let rec typecheck (e : expr) : exprtype =
+  match e with
+  | True -> BoolT
+  | False -> BoolT
+  | Zero -> NatT
+  | Succ(e) | Pred(e) -> (
+    match typecheck e with
+    | NatT -> NatT
+    | BoolT -> raise_type_error "Bool" "Nat" e
+    )
+  | IsZero(e) -> (
+    match typecheck e with
+    | NatT -> BoolT
+    | BoolT -> raise_type_error "Bool" "Nat" e
+    )
+  | Not(e) -> (
+    match typecheck e with
+    | BoolT -> BoolT
+    | NatT -> raise_type_error "Nat" "Bool" e
+    )
+  | And(e1, e2) | Or(e1, e2) -> (
+    let t1 = typecheck e1 in
+    let t2 = typecheck e2 in
+    match t1, t2 with
+    | BoolT, BoolT -> BoolT
+    | BoolT, NatT -> raise_type_error "Nat" "Bool" e2
+    | NatT, BoolT -> raise_type_error "Nat" "Bool" e1
+    | NatT, NatT -> raise_type_error "Nat" "Bool" e
+    )
+  | If(e1, e2, e3) -> (
+    let t1 = typecheck e1 in
+    let t2 = typecheck e2 in
+    let t3 = typecheck e3 in
+    match t1 with
+    | BoolT ->
+      if t2 = t3 
+        then t2
+      else 
+        raise (TypeError ("Branches of If have different types"))
+    | NatT -> raise_type_error "Nat" "Bool" e1
+    )
+
